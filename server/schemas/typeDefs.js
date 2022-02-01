89bc1cebdc4f7d5951cc377c41b2d05c7bf7b5b6
danny p <danny@dannys-MacBook-Pro.local>
2022-02-01 15:35:11 -0500

    updated graphql queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+
 // create our typeDefs
 const typeDefs = gql`
 
@@ -10,7 +11,20 @@ type Thought {
   createdAt: String
   username: String
   reactionCount: Int
-}`;
+  reactions: [Reaction]
+} 
+
+type Reaction {
+  _id: ID
+  reactionBody: String
+  createdAt: String
+  username: String
+}
+
+type Query {
+  thoughts(username: String): [Thought]
+}
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
