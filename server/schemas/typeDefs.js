2ec5a39b867c4c500ebde0b8a83f13fe09b58807
danny p <danny@dannys-MacBook-Pro.local>
2022-02-08 11:52:40 -0500

    added front end home page, just thoughts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,6 +31,7 @@ type Reaction {
 }
 
 type Query {
+  me: User
   users: [User]
   user(username: String!): User
   thoughts(username: String): [Thought]
@@ -40,6 +41,9 @@ type Query {
 type Mutation {
   login(email: String!, password: String!): Auth
   addUser(username: String!, email: String!, password: String!): Auth
+  addThought(thoughtText: String!): Thought
+  addReaction(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
 }
 
 type Auth {
